a2ca865d451692b173200a80a07b8e1f61370866
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-21 17:19:32 +0300

    added task3

---
+++ task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 38: Задайте с клавиатуры массив вещественных (double) чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+double [] array = new double[5];
+ ;
+for(int i = 0; i <array.Length; i++)
+{
+       Console.WriteLine($"Введите {i+1} элемент массива: ");
+    array[i] = Convert.ToDouble(Console.ReadLine());
+    
+}
+Console.WriteLine('[' + string.Join(", ",array) +']');
+
+
+double max = array[0];
+double min = array[0];
+for(int i = 1; i < array.Length; i++)
+{
+    if(array[i]>max) max = array[i];
+    if(array[i]<min) min = array[i]; 
+}
+
+Console.WriteLine($"Разность между максимальным = {max} и минимальным = {min} элементами массива равна {max-min}");
